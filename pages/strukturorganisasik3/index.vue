86f93fad5331a7cf90d6716a7a821628b7d65dd9
aidahdzfs <aidahzulfadilah@gmail.com>
2023-12-11 19:28:42 +0800

    update

--- pages/strukturorganisasik3/index.vue
+++ pages/strukturorganisasik3/index.vue
@@ -6,17 +6,111 @@
                 <img class="w-[100%] left-0 top-2 absolute" src="../assets/image/jkcenterhlm.png" />
                 <div class="w-[1439px] h-[201px] left-0 top-[1px] absolute bg-blue-400 bg-opacity-30"></div>
                 <div class="w-[474px] h-[45px] relative top-20 left-20">
-                    <a href="#" class="w-[302px] h-[45px] left-[172px] absolute text-slate-300 text-2xl font-medium">Struktur Organisasi K3</a>
-                    <a href="#" class="w-[172px] h-[45px] left-0 absolute text-center text-sky-950 text-2xl font-medium">Halaman</a>
-                    <a href="#" class="w-[180px] h-[45px] left-16 pt-1 absolute text-center text-sky-950 text-xl font-medium">  > </a>
+                    <a href="#" class="w-[302px] h-[45px] left-[172px] absolute text-slate-300 text-2xl font-semibold">Struktur Organisasi K3</a>
+                    <a href="#" class="w-[172px] h-[45px] left-0 absolute text-center text-sky-950 text-2xl font-semibold">Halaman</a>
+                    <a href="#" class="w-[180px] h-[45px] left-16 pt-1 absolute text-center text-sky-950 text-xl font-semibold">  > </a>
                 </div>
             </div>
         </section>
         <!-- header end -->
 
-        <section class="pt-8">
-            <div class="ml-28 text-black text-3xl font-bold">STRUKTUR ORGANISASI K3
-                <img src="../assets/image/Rectangle19.png" class="ml-100">
+        <section class="my-10 ml-28 py-8">
+            <div class="mb-16">
+                <div class="mb-5">
+                    <h1 class="text-black text-3xl font-bold pb-2">STRUKTUR ORGANISASI K3</h1>
+                    <img src="../assets/image/Rectangle19.png">
+                </div>
+                <ul class="list-outside list-decimal ml-10 text-left space-y-3 text-sm mt-4">
+                    <li class="font-semibold">Penanggung Jawab/ Ketua : <br>
+                        <span class="font-medium">Prof. Dr. Eng. Ir. Muhammad Isran Ramli, S.T., M.T., IPM., ASEAN.Eng</span>
+                    </li>
+                    <li class="font-semibold">Sekretaris : <br>
+                        <span class="font-medium">Dr.Eng. Ir. Ilham Bakri, ST., M.Sc., IPM., ASEAN Eng.</span>
+                    </li>
+                    <li class="font-semibold">Pengendali Dokumen SMK3/Audit Internal : <br>
+                        <span class="font-medium">Dr. Eng Irwan Ridwan Rahim, ST., MT</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="mb-16">
+                <div class="mb-5">
+                    <h1 class="text-black text-3xl font-bold pb-2">BIDANG - BIDANG</h1>
+                    <img src="../assets/image/Rectangle19.png">
+                </div>
+                <ul class="list-outside list-decimal ml-10 text-left space-y-3 text-sm mt-4">
+                    <li class="font-semibold">Keselamatan, Tanggap Darurat & Investigasi Kecelakaan <br>
+                        <span class="font-medium">Ketua: Dr.Eng. Ilham Alimuddin, M.GIS</span>
+                    </li>
+                    <li class="font-semibold">Kesehatan Kerja & P3K <br>
+                        <span class="font-medium">Ketua: Ir. Diniary Ikasari, ST., MT</span>
+                    </li>
+                    <li class="font-semibold">Perlengkapan K3 & Safety Patrol <br>
+                        <span class="font-medium">Ketua: Dra. Rahmawita, M.M</span>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <div class="mb-5">
+                    <h1 class="text-black text-3xl font-bold pb-2">UNIT - UNIT</h1>
+                    <img src="../assets/image/Rectangle19.png">
+                </div>
+                <ul class="list-outside list-decimal ml-10 text-left space-y-3 text-sm mt-4">
+                    <li class="font-semibold">Departemen Teknik Sipil <br>
+                        <span class="font-medium">       Ketua: Prof. Dr. Muh. Wihardi Tjaronge, S.T., M.Eng</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Lingkungan <br>
+                        <span class="font-medium">Ketua: Dr.Eng. Muralia Hustim, S.T., M.T.</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Mesin <br>
+                        <span class="font-medium">Ketua: Prof. D.Eng. Jalaluddin, S.T., M.T.</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Industri <br>
+                        <span class="font-medium">Ketua: Ir. Kifayah Amar, S.T., M.Sc., Ph.D.</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Geologi <br>
+                        <span class="font-medium">Ketua: Dr.Eng. Hendra Pachri, S.T., M.Eng</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Pertambangan <br>
+                        <span class="font-medium">Ketua: Dr. Aryanti Virtanti Anas, S.T., M.T.</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Perkapalan <br>
+                        <span class="font-medium">Ketua: Dr.Eng. Suandar Baso, S.T., M.T.</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Sistem Perkapalan <br>
+                        <span class="font-medium">Ketua: D.Eng. Faisal Mahmuddin, S.T., M.Inf., Tech, M.Eng.</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Kelautan <br>
+                        <span class="font-medium">Ketua: Dr. Ir. Chairul Paotonan, S.T., M.T.</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Arsitektur <br>
+                        <span class="font-medium">Ketua: Dr. Edward Syarif, S.T., M.T.</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik PWK <br>
+                        <span class="font-medium">Ketua: Dr.Eng. Abdul Rachman Rasyid, S.T., M.Si.</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Elektro <br>
+                        <span class="font-medium">Ketua: Dr.Eng. Ir. Dewiani, M.T.</span>
+                    </li>
+                    <li class="font-semibold">Departemen Teknik Informatika <br>
+                        <span class="font-medium">Ketua: Prof.Dr. Ir. Indrabayu, S.T., M.T., M.Bus.Sys.</span>
+                    </li>
+                    <li class="font-semibold">Classroom (CR) <br>
+                        <span class="font-medium">Ketua: Dr. Amil Ahmad Ilham, S.T., MIT.</span>
+                    </li>
+                    <li class="font-semibold">CSA & COT <br>
+                        <span class="font-medium">Ketua: Dr.Eng. Ir. Muhammad Rusman, S.T., M.T.</span>
+                    </li>
+                    <li class="font-semibold">Workshop <br>
+                        <span class="font-medium">Ketua: Dr. Rustan Tarakka, ST., M.T.</span>
+                    </li>
+                    <li class="font-semibold">Fasilitas Penunjang Lainnya <br>
+                        <span class="font-medium">Ketua: Ir. Abdul Mufti Radja, S.T., M.T., Ph.D</span>
+                    </li>
+                    <li class="font-semibold">Fasilitas Asrama <br>
+                        <span class="font-medium">Ketua: Dr.Eng. Ir. Muhammad Ramli, M.T</span>
+                        <span class="font-medium">Sekretaris: Dr. Sultan, S.T., M.T</span>
+                    </li>
+                </ul>
             </div>
         </section>
     </main>
